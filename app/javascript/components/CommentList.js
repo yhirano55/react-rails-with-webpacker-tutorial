3a94998b728eb1d2468d7049a2217649da6a1b2a
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-05-25 12:09:15 +0900

    Modify CommentList component

--- app/javascript/components/CommentList.js
+++ app/javascript/components/CommentList.js
@@ -1,8 +1,14 @@
 var React = require("react")
+var Comment = require("./Comment")
 var CommentList = React.createClass({
-
   render: function() {
-    return <div />;
+    return (
+      <div className="CommentList">
+        <Comment author="alice">My name is Alice.</Comment>
+        <Comment author="bob">My name is Bob.</Comment>
+        <Comment author="carol">My name is Carol.</Comment>
+      </div>
+    );
   }
 });
 module.exports = CommentList
