bad36c1db02edd1d351b2d688bcb6b5274565565
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-05-25 12:13:05 +0900

    rails g controller Api::V1::Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'comments/index'
+    end
+  end
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
