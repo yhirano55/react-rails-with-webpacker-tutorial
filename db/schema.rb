54e6198b305ad6586a5c01d4d077b25808dc76fc
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-05-25 12:12:24 +0900

    rails g model Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170525031209) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
